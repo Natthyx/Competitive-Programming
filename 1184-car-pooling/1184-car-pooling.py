9a441802f32cc7cb2f95c6ac5cba5f878d1b2a21
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-02-21 06:12:40 -0800

    Time: 0 ms (100%), Space: 18.4 MB (12.75%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,19 +1,21 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        suffixsum = [0] * 1001
-        maxi = 0
-        
-        for num_passengers, start, end in trips:
-            suffixsum[start] += num_passengers
-            suffixsum[end] -= num_passengers
-            maxi = max(maxi, end)
-        
-        if suffixsum[0] > capacity:
+        prefix = [0]*1001
+        mx = 0
+        mn = float("inf")
+        for num, begin, des in trips:
+    
+            prefix[begin]+= num
+            prefix[des]-= num
+            mx = max(mx,des)
+        if prefix[0] > capacity:
             return False
-        
-        for i in range(1, maxi + 1):
-            suffixsum[i] += suffixsum[i - 1]
-            if suffixsum[i] > capacity:
+        for i in range(1,mx+1):
+            prefix[i] += prefix[i-1]
+            if prefix[i] > capacity:
                 return False
-        
+
         return True
+           
+
+        
