330a7454c3a66efd5174c6ac52417e7abefd94a8
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-03-01 21:03:53 +0300

    Time: 109 ms (9.76%), Space: 28.3 MB (91.12%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -3,19 +3,14 @@ class Solution:
         n = len(nums)
         prefixsm = nums[0]
         mx = nums[0]
-
-        for i in range(1,n):
-            prefixsm = max(prefixsm+nums[i], nums[i])
-
-            mx = max(mx,prefixsm)
-
-        
         prefixm = nums[0]
         mn = nums[0]
 
         for i in range(1,n):
+            prefixsm = max(prefixsm+nums[i], nums[i])
             prefixm = min(prefixm+nums[i], nums[i])
             mn = min(mn,prefixm)
+            mx = max(mx,prefixsm)
 
 
         return max(mx,abs(mn))
