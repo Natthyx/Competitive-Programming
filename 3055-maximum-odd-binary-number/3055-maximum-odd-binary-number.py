33ca7d011bb9238143092855099c3c21567a18c2
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-03-06 01:49:22 +0300

    Time: 0 ms (100%), Space: 17.7 MB (62.43%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
         s = list(s)
-        s.sort()
-        n = len(s)
-        ans = s[:n-1]
-
-        ans.sort(reverse=True)
-
-        ans = "".join(ans)
-        ans+="1"
-        return ans
+        s.sort(reverse=True)
+        return "".join(s[1:]+["1"])
+        
         
