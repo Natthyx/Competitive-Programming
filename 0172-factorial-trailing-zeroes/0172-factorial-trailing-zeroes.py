3a03170ad30e60b75e67303b753bc75edbf4b1b0
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-03-10 22:21:22 -0700

    Time: 7662 ms (11.91%), Space: 17.9 MB (18.3%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,5 +1,25 @@
 class Solution:
+    def factorial(self, num):
+            if num == 0:
+                return 0
+            
+            k = factorial(num-1)
+            return num * k
     def trailingZeroes(self, n: int) -> int:
         if n == 0:
             return 0
-        return n //5 + self.trailingZeroes(n//5)
+        ans = self.factorial(n)
+        count = 0
+
+
+        while ans > 0:
+            if ans%10 == 0:
+                count+=1
+                ans= ans // 10
+            else:
+                break
+
+        return count
+
+
+        
