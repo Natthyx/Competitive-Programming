ee3bc8c75c7dedca3651fd102756184420c11f4e
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-04-14 19:18:33 +0300

    Time: 0 ms (100%), Space: 17.8 MB (66.18%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,29 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        left = []
-        right = []
-        if not p and not q:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:        
+        def bfs(queue,store):
+            while queue:
+                node = queue.popleft()
+                store.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                elif not node.left:
+                    store.append(None)
+                elif not node.right:
+                    store.append(None)
+
+        p_store = []
+        q_store = []
+
+        if not p and (not q):
             return True
-
-        def dfs(node, arr):
-            if not node:
-                arr.append(None)
-                return
-            arr.append(node.val)
-            dfs(node.left, arr)
-            dfs(node.right, arr)
-
-            return arr
-
-        if not p or not q:
+        if not p and q or (p and (not q)):
             return False
+        p_queue = deque([p])
+        q_queue = deque([q])
+        bfs(p_queue,p_store)
+        bfs(q_queue,q_store)
 
-        if not p and not q:
-            return True
-
-        k = dfs(p,left)
-        l = dfs(q,right)
-
-        return k == l
+        return p_store == q_store
