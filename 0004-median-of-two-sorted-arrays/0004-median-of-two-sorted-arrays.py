cf382423f6cbc28473655a51cc9097d6b8d44ad2
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-04-21 11:26:16 +0300

    Time: 7 ms (17.56%), Space: 18.2 MB (40.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,20 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1.sort()
-        n= len(nums1)
-        if n % 2 ==1:
-            return nums1[n//2]
-        else:
-            mid1 = nums1[n//2 - 1]
-            mid2 = nums1[n//2]
-            return (mid1+mid2)/2
-            
-            
-            
-            
-            
-            
-            
-            
-        
+        if len(nums1) > len(nums2):
+            nums1 , nums2 = nums2,nums1
+        total = len(nums1)+len(nums2)
+        half = total//2
+
+        l , r = 0 , len(nums1)-1
+
+        while True:
+            midA = (l+r)//2
+            midB = half - midA - 2
+            Aleft = nums1[midA] if midA >= 0 else float("-inf")
+            Aright = nums1[midA+1] if midA+1 < len(nums1) else float("inf")
+            Bleft = nums2[midB] if midB >= 0 else float("-inf")
+            Bright = nums2[midB+1] if midB+1 < len(nums2) else float("inf")
+
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                else:
+                    return (max(Aleft,Bleft) + min(Bright,Aright)) / 2.0
+            elif Aleft > Bright:
+                r = midA - 1
+            else:
+                l = midA + 1
+   
