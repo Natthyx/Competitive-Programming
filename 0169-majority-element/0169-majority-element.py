2d58b76b29e97a1e881583335eb935bda26a205b
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-04-27 17:13:00 +0300

    Time: 3 ms (82.27%), Space: 19.2 MB (87.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        counter = defaultdict(int)
-        
-        for i in range(n):
-            counter[nums[i]]+=1
-        for key, value in counter.items():
-            if value > (n/2):
-                return key
-            
-            
-        
-        
-        
-            
+        # boyer-moore-vomiting-algorithm
+
+        candidate = 0
+        count = 0
+
+        for num in nums:
+            # if count is zero, we will consider the current element as a candidate
+            if count == 0:
+                candidate = num
+            # if current element same with candidate, we will increment the count by one, else decrement
+            if num == candidate:
+                count+=1
+            else:
+                count-=1
+        # because it is guarantee that the element exists it will be one element left after all cancellation
+        return candidate
