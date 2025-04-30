20ef7101d6f3083e28367119d1a9f2669794033d
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-04-30 20:59:57 +0300

    Time: 17 ms (56.27%), Space: 23.6 MB (84.66%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -0,0 +1,14 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
