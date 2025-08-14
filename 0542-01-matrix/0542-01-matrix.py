b519ce6f72cd4dfbcce318877570908238d458c4
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2025-08-14 08:43:17 +0300

    Time: 179 ms (34.52%), Space: 20.4 MB (70.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,31 +1,38 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        row , col = len(mat), len(mat[0])
+        '''
+        0 0 0
+        0 1 0
+        1 2 1
 
-        def inbound(row,col):
-            return 0 <= row < len(mat) and 0 <= col < len(mat[0])
+        '''
 
         direction = [(0,1),(1,0),(-1,0),(0,-1)]
-
-        visited = [[False] * col for _ in range(row)]
+        def inbound(row,col):
+            return 0 <= row < len(mat) and 0 <= col < len(mat[0])
+        
+        visited = [[False]*len(mat[0]) for _ in range(len(mat))]
 
         queue = deque()
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
                 if mat[i][j] == 0:
-                    queue.append((i,j,0))
+                    queue.append([i,j,0])
                     visited[i][j] = True
-
+        
         while queue:
-            roww, coll , step = queue.popleft()
+            for _ in range(len(queue)):
+                row , col , step = queue.popleft()
 
-            for r, c in direction:
-                new_row , new_col = roww + r , coll + c
-                if inbound(new_row,new_col) and (not visited[new_row][new_col]):
-                    visited[new_row][new_col] = True
-                    mat[new_row][new_col] = step + 1
-                    queue.append((new_row,new_col,step+1)) 
-                    
+                for r , c in direction:
+                    new_row = row + r
+                    new_col = col + c
 
+                    if inbound(new_row,new_col) and not visited[new_row][new_col]:
+                        queue.append([new_row, new_col , step + 1])
+                        mat[new_row][new_col] = step + 1
+                        visited[new_row][new_col] = True
+        
         return mat
+
